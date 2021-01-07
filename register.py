db4f0161f2e79cb82169a39c0f804b83e30c812a
Bilal-khursheed <bilal.khursheed617@gmail.com>
2021-01-07 17:16:54 +0500

    testing reg py

--- register.py
+++ register.py
@@ -20,15 +20,22 @@ def append_list_as_row(file_name, list_of_elem):
         csv_writer.writerow(list_of_elem)
 
 
-inputfilepath = sys.argv[1]
-Name = sys.argv[2]
-Gender = sys.argv[3]
-Age = sys.argv[4]
-Time = sys.argv[5]
-Address = sys.argv[6]
-Wear = sys.argv[7]
-Phone = sys.argv[8]
-
+# inputfilepath = sys.argv[1]
+# Name = sys.argv[2]
+# Gender = sys.argv[3]
+# Age = sys.argv[4]
+# Time = sys.argv[5]
+# Address = sys.argv[6]
+# Wear = sys.argv[7]
+# Phone = sys.argv[8]
+inputfilepath = "https://res.cloudinary.com/dztyioznk/image/upload/v1609965455/reportPics/ivcorepehstercg8uqtt.png"; 
+Name = "Bilal"
+Gender = "Male"
+Age = "22"
+Time = "22/2020"
+Address = "Mirpur Ajk"
+Wear = "Black Shirt with white pent"
+Phone = "03435527709"
 response = urllib.request.urlopen(inputfilepath)
 readimage = face_recognition.load_image_file(response)
 Encodings = face_recognition.face_encodings(readimage)[0]
